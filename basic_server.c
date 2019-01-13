0d7f8827937890f4bce6e9d07ee44c0f6bde588e
Stefan Tan <stan3@stuy.edu>
2019-01-13 01:10:12 -0500

    completed assigning roles

--- basic_server.c
+++ basic_server.c
@@ -1,41 +1,7 @@
 #include "pipe_networking.h"
-#include <signal.h>
 
 //void process(char *s);
 void subserver(int from_client);
-
-
-/*static void sighandler(int signo) {
-  if (signo == SIGINT) {
-      remove("wkp");
-      exit(0);
-  }
-}
-
-int main() {
-  signal(SIGINT, sighandler);
-
-  int to_client;
-  int from_client;
-  int LIMIT = 10;
-  int mafialimit = 2;
-  int curnorm = 0;
-  int curmafia = 0;
-  int day_night = 0;
-  
-  
-  while(1) {
-    from_client = server_handshake( &to_client );
-    
-    
-    
-    while(curmafia && read(from_client, data, BUFFER_SIZE)) {
-      printf("Server %d received: %s\n", getpid(), data);
-      write(to_client, data, BUFFER_SIZE);
-    }
-  }
-}
-*/
 
 int main() {
 
@@ -56,20 +22,64 @@ int main() {
 
 void subserver(int client_socket) {
   //int LIMIT = 10;
-  int mafialimit = 2;
-  int curnorm = 0;
-  int curmafia = 0;
+  int civil_left = 0;
+  int mafia_left = 0;
   int day_night = 0;
   int num_players = 0;
-  int curr_player = 0; 
   char buffer[BUFFER_SIZE];
   
   //Reads the number of players
   read(client_socket, buffer, sizeof(buffer));
   num_players = atoi(buffer);
-  
-       
 
+  //Assigning roles
+  strcpy(buffer, "The Server is now assigning roles...");
+  write(client_socket, buffer, sizeof(buffer));
+  
+  int mafia0 = rand() % num_players;
+  int mafia1 = rand() % num_players;
+  int police = rand() % num_players;
+  while (mafia0 == mafia1) {
+    mafia1 = rand() % num_players;
+  }
+  while (police == mafia0 && police == mafia1) {
+    police = rand() % num_players;
+  }
+
+  mafia_left = 2;
+  civil_left = num_players - mafia_left;
+
+  int i = 0; 
+  while(read(client_socket, buffer, sizeof(buffer)) && i < num_players) {
+    if (i == mafia0) {
+      char other_mafia[2];
+      sprintf(other_mafia, "%d", mafia1);
+      strcpy(buffer, "You are a member of the mafia.\n Goal: Work together to eliminate all non-mafia players before they find out!\n Other Mafia Member: ");
+      strcat(buffer, other_mafia);
+      write(client_socket, buffer, sizeof(buffer));
+      i++;
+    }
+    if (i == mafia1) {
+      char other_mafia[2];
+      sprintf(other_mafia, "%d", mafia0);
+      strcpy(buffer, "You are a member of the mafia.\n Goal: Work together to eliminate all non-mafia players before they find out!\n Other Mafia Member: ");
+      strcat(role, other_mafia);
+      write(client_socket, buffer, sizeof(buffer));
+      i++;
+    }
+    if (i == police) {
+      strcpy(buffer, "You are the police.\n Goal: Find and arrest all the member of the mafia before it's too late!");
+      write(client_socket, buffer, sizeof(buffer));
+      i++;
+    }
+    else {
+      strcpy(buffer, "You are a civilian.\n Goal: Work together with other civilians to get rid of the mafia members hiding within your community!");
+      write(client_socket, buffer, sizeof(buffer));
+      i++;
+    }
+  }
+      
+  
   /*  while (read(client_socket, buffer, sizeof(buffer))) {
 
     printf("[subserver %d] received: [%s]\n", getpid(), buffer);
--- client.c
+++ client.c
@@ -1,28 +1,5 @@
 #include "pipe_networking.h"
 
-
-/*int main() {
-
-  int to_server;
-  int from_server;
-
-  from_server = client_handshake( &to_server );
-
-  char w[BUFFER_SIZE];
-  char r[BUFFER_SIZE];
-
-  while(1){
-    printf("Enter data: ");
-    fgets(w, BUFFER_SIZE, stdin);
-    w[strlen(w) - 1] = '\0';
-
-    write(to_server, w, BUFFER_SIZE);
-    read(from_server, r, BUFFER_SIZE);
-    printf("Server's Response: %s\n", r);
-  }
-}
-*/
-
 int main(int argc, char **argv) {
 
   int server_socket;
@@ -33,11 +10,36 @@ int main(int argc, char **argv) {
   else
     server_socket = client_setup( TEST_IP );
 
-  printf("Enter the number of players(There can't be more than 10 players): ");
+  //Setting up the Game
+  int num_players = 0;
+  
+  printf("Enter the number of players(5-10 players Only!): ");
   fgets(buffer, sizeof(buffer), stdin);
   *strchr(buffer, '\n') = 0;
+  num_players = atoi(buffer);
   write(server_socket, buffer, sizeof(buffer));
   
+  //Receiving roles
+  read(server_socket, buffer, sizeof(buffer));
+  printf("%s\n", buffer);
+
+  for (int i = 0; i < num_players; i++) {
+    printf("Welcome Player_%d!\n", i);
+    printf("To confirm that you are Player_%d, please enter 'y': ", i);
+    fgets(buffer, sizeof(buffer), stdin);
+    if (!strcmp(buffer, "y\n")) {
+      write(server_socket, buffer, sizeof(buffer));
+      read(server_socket, buffer, sizeof(buffer));
+      printf("\nCONFIDENTIAL INFO!\n\n %s\n\n", buffer);
+      printf("Enter 'y' if understood: ");
+      fgets(buffer, sizeof(buffer), stdin);
+      if (!strcmp(buffer, "y\n")) {
+	execlp("clear", "clear", NULL);
+	printf("Switch to the next player.");
+      }
+    }
+  }
+      
   /*
   while (1) {
     printf("enter data: ");
