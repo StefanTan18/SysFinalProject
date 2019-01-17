6969921a36781e967791336ce458e2673d04fa26
djiang2001 <djiang2@stuy.edu>
2019-01-16 23:08:50 -0500

    working on freeze issue

--- client.c
+++ client.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
   //Setting up the Game
   int num_players = 0;
 
-  printf("Enter the number of players(6-10 players Only!): ");
+  printf("Enter the number of players(6-10 players Only!): \n");
   fgets(buffer, sizeof(buffer), stdin);
   num_players = atoi(buffer);
   *strchr(buffer, '\n') = 0;
@@ -35,38 +35,38 @@ int main(int argc, char **argv) {
   int votes[num_players]; //store the votes
   int your_choice = 0; //placed into vote
   // int players[num_players];
-  
+
   //Receiving roles
   read(server_socket, buffer, sizeof(buffer));
   printf("%s\n", buffer);
 
   for (int i = 0; i < num_players; i++) {
     printf("Welcome Player_%d!\n", i);
-    printf("To confirm that you are Player_%d, please enter 'y': ", i);
+    printf("To confirm that you are Player_%d, please enter 'y': \n", i);
     fgets(buffer, sizeof(buffer), stdin);
     if (!strcmp(buffer, "y\n")) {
       *strchr(buffer, '\n') = 0;
       write(server_socket, buffer, sizeof(buffer));
       read(server_socket, buffer, sizeof(buffer));
-      //  players[i] = 
+      //  players[i] =
       printf("\nCONFIDENTIAL INFO!\n\n %s\n\n", buffer);
-      printf("Enter 'y' if understood: ");
+      printf("Enter 'y' if understood: \n");
       fgets(buffer, sizeof(buffer), stdin);
       if (!strcmp(buffer, "y\n")) {
         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    printf("Switch to the next player.\n");
+        printf("Switch to the next player.\n");
       }
     }
   }
 
-  printf("Enter 'y' if you are ready to start mafia? ");
+  printf("Enter 'y' if you are ready to start mafia.\n ");
   fgets(buffer, sizeof(buffer), stdin);
   if (!strcmp(buffer, "y\n")) {
     int i = 0;
     while(i < num_players) {
       printf("Welcome Player_%d!\n", i);
-      printf("To confirm that you are Player_%d, please enter 'y': ", i);
-      fgets(buffer,BUFFER_SIZE, stdin);
+      printf("To confirm that you are Player_%d, please enter 'y': \n", i);
+      fgets(buffer,sizeof(buffer), stdin);
       if (!strcmp(buffer, "y\n")) {
         sprintf(buffer, "%d\n", i);
         *strchr(buffer, '\n') = 0;
@@ -85,18 +85,18 @@ int main(int argc, char **argv) {
             break;
           }
         }
-        else if (!strcmp(buffer, "It's nighttime in the community and you are currently sleeping.")) {
-          printf("Let the vote commence! Who do you believe is the mafia member? (0-%d)", num_players - 1);
+        else if (!strcmp(buffer, "It's nighttime in the community and you are currently sleeping.\n")) {
+          printf("Let the vote commence! Who do you believe is the mafia member? (0-%d)\n", num_players - 1);
           fgets(buffer, sizeof(buffer), stdin);
           your_choice = atoi(buffer);
           votes[your_choice] += 1;
 
           if (i == num_players - 1) {
-            printf("Enter 'y' if done: ");
+            printf("Enter 'y' if done: \n");
             fgets(buffer, sizeof(buffer), stdin);
             if (!strcmp(buffer, "y\n")) {
-          printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-          printf("All players can see this!\n");
+              printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+              printf("All players can see this!\n");
               printf("The vote has concluded.\n");
               printf("The majority has decided that Player_%d should be executed.\n", getMajority(votes,num_players));
               printf("Player_%d has been executed.\n", getMajority(votes,num_players));
@@ -105,17 +105,17 @@ int main(int argc, char **argv) {
               write(server_socket, buffer, sizeof(buffer));
               read(server_socket, buffer, sizeof(buffer));
               printf("%s\n", buffer);
-              if (!strcmp(buffer, "THE MAFIA HAS BEEN EXECUTED!")) {
+              if (!strcmp(buffer, "THE MAFIA HAS BEEN EXECUTED!\n")) {
                 break;
               }
             }
           }
         }
-        printf("Enter 'y' if done: ");
+        printf("Enter 'y' if done: \n");
         fgets(buffer, sizeof(buffer), stdin);
         if (!strcmp(buffer, "y\n")) {
       printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-      printf("Switch to the next player.");
+      printf("Switch to the next player.\n");
           i++;
           if (i == num_players) {
             i = 0;
--- forking_server.c
+++ forking_server.c
@@ -8,26 +8,26 @@ int main() {
   int listen_socket;
   int f;
   listen_socket = server_setup();
- 
+
   while (1) {
 
     int client_socket = server_connect(listen_socket);
     f = fork();
     if (f == 0) subserver(client_socket);
     else close(client_socket);
-  } 
+  }
 }
 
 void subserver(int client_socket) {
-  
+
   //incrementing shmem
   /* int key = 99;
   int shmid = shmget(key, sizeof(int), NULL);
   int *num =(int *) shmat(shmid, NULL, 0);
   *num = (*num) + 1;
-  shmdt(num);    
+  shmdt(num);
   */
-  
+
   //int LIMIT = 10;
   int civil_left = 0;
   int mafia_left = 0;
@@ -43,7 +43,7 @@ void subserver(int client_socket) {
   num_players = atoi(buffer);
 
   //Assigning roles
-  strcpy(buffer, "The Server is now assigning roles...");
+  strcpy(buffer, "The Server is now assigning roles...\n");
   write(client_socket, buffer, sizeof(buffer));
 
   int mafia = rand() % num_players;
@@ -59,54 +59,56 @@ void subserver(int client_socket) {
       i++;
     }
     else {
-      strcpy(buffer, "You are a civilian.\nGoal: Work together with other civilians to get rid of the 1 mafia member hiding within your community!");
+      strcpy(buffer, "You are a civilian.\nGoal: Work together with other civilians to get rid of the 1 mafia member hiding within your community!\n");
       write(client_socket, buffer, sizeof(buffer));
       i++;
     }
   }
 
   //Game
-  while (mafia_left && civil_left != 1) {
+  while (mafia_left && civil_left > 1) {
     read(client_socket, buffer, sizeof(buffer));
     curr_player = atoi(buffer);
-    if (day_night) {
+    if (!day_night) {
+
       if (curr_player == mafia){
-        strcpy(buffer, "It's nighttime in the community. Time to eliminate a civilian!");
+        strcpy(buffer, "It's nighttime in the community. Time to eliminate a civilian!\n");
         write(client_socket, buffer, sizeof(buffer));
         read(client_socket, buffer, sizeof(buffer));
         recently_killed = atoi(buffer);
         civil_left--;
       }
       else {
-        strcpy(buffer, "It's nighttime in the community and you are currently sleeping.");
-          write(client_socket, buffer, sizeof(buffer));
+        strcpy(buffer, "It's nighttime in the community and you are currently sleeping.\n");
+        write(client_socket, buffer, sizeof(buffer));
       }
       day_night++;
+
     }
-    else if (day_night == 1) {
+    else if (day_night) {
       curr_day++;
       sprintf(buffer, "It's day number %d in our beautiful community. Unfortunately Player_%d has been killed by the mafia.\n", curr_day, recently_killed);
       write(client_socket, buffer, sizeof(buffer));
       read(client_socket, buffer, sizeof(buffer));
       if (atoi(buffer) == mafia) {
         mafia_left--;
-        strcpy(buffer, "THE MAFIA HAS BEEN EXECUTED!");
+        strcpy(buffer, "THE MAFIA HAS BEEN EXECUTED!\n");
         write(client_socket, buffer, sizeof(buffer));
       }
       else {
         civil_left--;
-        strcpy(buffer, "AN INNOCENT CIVILIAN HAS BEEN EXECUTED!");
+        strcpy(buffer, "AN INNOCENT CIVILIAN HAS BEEN EXECUTED!\n");
         write(client_socket, buffer, sizeof(buffer));
       }
     }
   }
 
   if (mafia_left == 0) {
-    strcpy(buffer, "Justice has prevailed! The mafia member has been executed and peace has returned to our community. GOOD WORK!");
+    strcpy(buffer, "Justice has prevailed! The mafia member has been executed and peace has returned to our community. GOOD WORK!\n");
     write(client_socket, buffer, sizeof(buffer));
   }
   else if (civil_left == 1) {
-    strcpy(buffer, "One civilian left alone against one mafia member. That is not going to end up well. WELL DONE TO THE MAFIA!");
+    strcpy(buffer, "One civilian left alone against one mafia member. That is not going to end up well. WELL DONE TO THE MAFIA!\n");
     write(client_socket, buffer, sizeof(buffer));
   }
 
