783c565922362f96e7056050aba22a0686267e45
rscheinberg <rscheinberg@stuy.edu>
2019-01-18 18:24:28 -0500

    added error checking to input

--- client.c
+++ client.c
@@ -26,6 +26,14 @@ int main(int argc, char **argv) {
   printf("Enter the number of players(6-10 players Only!): \n");
   fgets(buffer, sizeof(buffer), stdin);
   num_players = atoi(buffer);
+  
+  //Checks input number for correct value
+  while(num_players < 6 || num_players > 10) {
+    printf("Input invalid.  Please enter a correct value (6-10): ");
+    fgets(buffer, sizeof(buffer), stdin);
+    num_players = atoi(buffer);
+  }     
+  
   *strchr(buffer, '\n') = 0;
   write(server_socket, buffer, sizeof(buffer));
 
@@ -44,65 +52,89 @@ int main(int argc, char **argv) {
     printf("Welcome Player_%d!\n", i);
     printf("To confirm that you are Player_%d, please enter 'y': \n", i);
     fgets(buffer, sizeof(buffer), stdin);
-    if (!strcmp(buffer, "y\n")) {
-      *strchr(buffer, '\n') = 0;
-      write(server_socket, buffer, sizeof(buffer));
-      read(server_socket, buffer, sizeof(buffer));
-      //  players[i] =
-      printf("\nCONFIDENTIAL INFO!\n\n %s\n\n", buffer);
-      printf("Enter 'y' if understood: \n");
+    buffer[0] = tolower(buffer[0]);
+    while(strcmp(buffer, "y\n")) {
+      printf("Waiting for player to confirm identity with 'y': ");
       fgets(buffer, sizeof(buffer), stdin);
-      if (!strcmp(buffer, "y\n")) {
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        printf("Switch to the next player.\n");
-      }
+      buffer[0] = tolower(buffer[0]);
     }
+    *strchr(buffer, '\n') = 0;
+    write(server_socket, buffer, sizeof(buffer));
+    read(server_socket, buffer, sizeof(buffer));
+    //  players[i] =
+    printf("\nCONFIDENTIAL INFO!\n\n %s\n\n", buffer);
+    printf("Enter 'y' if understood: \n");
+    fgets(buffer, sizeof(buffer), stdin);
+    while(strcmp(buffer, "y\n")) {
+      printf("Waiting for player to signal 'y': ");
+      fgets(buffer, sizeof(buffer), stdin);
+      buffer[0] = tolower(buffer[0]);
+    }
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("Switch to the next player.\n");
   }
 
   printf("Enter 'y' if you are ready to start mafia.\n ");
   fgets(buffer, sizeof(buffer), stdin);
-  if (!strcmp(buffer, "y\n")) {
-    int i = 0;
-    while(i < num_players) {
-      printf("Welcome Player_%d!\n", i);
-      printf("To confirm that you are Player_%d, please enter 'y':\n", i);
-      fgets(buffer,sizeof(buffer), stdin);
-      
-      if (!strcmp(buffer, "y\n")) {
-        sprintf(buffer, "%d\n", i);
-        *strchr(buffer, '\n') = 0;
-        write(server_socket, buffer, sizeof(buffer));
-        read(server_socket, buffer, sizeof(buffer));
-        printf("%s\n", buffer);
-	printf("%d",strncmp(buffer, "G", 1));
+  buffer[0] = tolower(buffer[0]);
+  while(strcmp(buffer, "y\n")) {
+    printf("Waiting for player to signal 'y': ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[0] = tolower(buffer[0]);
+  }
+  int i = 0;
+  while(i < num_players) {
+    printf("Welcome Player_%d!\n", i);
+    printf("To confirm that you are Player_%d, please enter 'y':\n", i);
+    fgets(buffer,sizeof(buffer), stdin);
+    while(strcmp(buffer, "y\n")) {
+      printf("Waiting for player to confirm identity with 'y': ");
+      fgets(buffer, sizeof(buffer), stdin);
+      buffer[0] = tolower(buffer[0]);
+    }
+    sprintf(buffer, "%d\n", i);
+    *strchr(buffer, '\n') = 0;
+    write(server_socket, buffer, sizeof(buffer));
+    read(server_socket, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+    printf("%d",strncmp(buffer, "G", 1));
 	
-        if (!strcmp(buffer, "It's nighttime in the community. Time to eliminate a civilian!\n")) {
-          printf("Select your target (0-%d)\n: ", num_players - 1);
-          fgets(buffer, sizeof(buffer), stdin);
-          recently_killed = atoi(buffer);
-          notalive[numDead] = recently_killed;
-          numDead++;
-          *strchr(buffer, '\n') = 0;
-          write(server_socket, buffer, sizeof(buffer));
-          if (numDead == num_players - 2) {
-            break;
-          }
-        }
-
-        else if (!strncmp(buffer, "G",1)) {
-          printf("Let the vote commence! Who do you believe is the mafia member? (0-%d)\n", num_players - 1);
-          fgets(buffer, sizeof(buffer), stdin);
-          your_choice = atoi(buffer);
-          votes[your_choice] += 1;
+    if (!strcmp(buffer, "It's nighttime in the community. Time to eliminate a civilian!\n")) {
+      printf("Select your target (0-%d)\n: ", num_players - 1);
+      fgets(buffer, sizeof(buffer), stdin);
+      recently_killed = atoi(buffer);
+      while(buffer[0] >= 'A' || recently_killed >= num_players || recently_killed < 0) {
+        printf("Invalid Input. Please select a valid player: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        recently_killed = atoi(buffer);
+      }     
+      
+      notalive[numDead] = recently_killed;
+      numDead++;
+      *strchr(buffer, '\n') = 0;
+      write(server_socket, buffer, sizeof(buffer));
+      if (numDead == num_players - 2) {
+        break;
+      }
+    }
+    else if (!strncmp(buffer, "G",1)) {
+      printf("Let the vote commence! Who do you believe is the mafia member? (0-%d)\n", num_players - 1);
+      fgets(buffer, sizeof(buffer), stdin);
+      your_choice = atoi(buffer);
+      votes[your_choice] += 1;
 
 	  /*
-          if (i == num_players - 1) {
-	    printf("nump %d", num_players);
-	    printf("i %d", i);
-	    
-            printf("Enter 'y' if done: \n");
+        if (i == num_players - 1) {
+	      printf("nump %d", num_players);
+	      printf("i %d", i);
+          printf("Enter 'y' if done: \n");
+          fgets(buffer, sizeof(buffer), stdin);
+          buffer[0] = tolower(buffer[0]);
+          while(strcmp(buffer, "y\n")) {
+            printf("Waiting for player to indicate they are finished with 'y': ");
             fgets(buffer, sizeof(buffer), stdin);
-	    if (!strcmp(buffer, "y\n")) {
+            buffer[0] = tolower(buffer[0]);
+          }
               printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
               printf("All players can see this!\n");
               printf("The vote has concluded.\n");
@@ -121,19 +153,23 @@ int main(int argc, char **argv) {
         }
 	
 	    */
-        printf("Enter 'y' if done: \n");
-        fgets(buffer, sizeof(buffer), stdin);
-        if (!strcmp(buffer, "y\n")) {
-      printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-      printf("Switch to the next player.\n");
-          i++;
-          if (i == num_players) {
-            i = 0;
-          }
-        }
-      }
+    }
+    printf("Enter 'y' if done: \n");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[0] = tolower(buffer[0]);
+    while(strcmp(buffer, "y\n")) {
+      printf("Waiting for player to signal they are finished with 'y': ");
+      fgets(buffer, sizeof(buffer), stdin);
+      buffer[0] = tolower(buffer[0]);
+    }
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("Switch to the next player.\n");
+    i++;
+    if (i == num_players) {
+      i = 0;
     }
   }
+
 
   printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   read(server_socket, buffer, sizeof(buffer));
